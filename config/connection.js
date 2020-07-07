8ff99b8b0fc081a7fde797b387832af60e4e18f9
Iva Trecakov <itreckov@gmail.com>
2020-07-07 01:06:49 -0400

    deployed

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,19 @@
 // Initialize Node.js package
 var mysql = require("mysql");
+var connection;
 
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        port: 3306,
+        host: "localhost",
+        user: "root",
+        password: "password",
+        database: "todoagain_db"
+    });
+};
 
-var connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "password",
-    database: "burgers_db"
-});
-
-// Connect to MySQL server
-connection.connect(function (error) {
-    if (error) {
-        console.error("MYSQL CONNECTION ERROR: " + error);
-    }
-    console.log("Connected to MySQL server.");
-});
-
+connection.connect();
 // Export connection
 module.exports = connection;
